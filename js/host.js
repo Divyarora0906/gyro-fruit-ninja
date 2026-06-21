1ea58c7f9c176c2cba6a0aaaf6a877e8b0e007db
Divyarora0906 <divyarora0906@gmail.com>
2026-06-22 02:28:39 +0530

    -fix: updated code of controller

--- js/host.js
+++ js/host.js
@@ -6,33 +6,63 @@ import {
   onSnapshot,
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-document.getElementById("createRoomBtn").addEventListener("click", createRoom);
-
-async function createRoom() {
-  const roomId = Math.random().toString(36).substring(2, 8).toUpperCase();
-
-  await setDoc(doc(db, "rooms", roomId), {
-    joined: false,
-    createdAt: Date.now(),
-  });
-  new QRCode(document.getElementById("qrcode"), joinUrl);
-
-  document.getElementById("roomId").textContent = roomId;
-}
-
-const joinUrl = `${location.origin}/controller.html?room=${roomId}`;
-console.log(joinUrl);
 const statusEl = document.getElementById("status");
 
-onSnapshot(doc(db, "rooms", roomId), (snapshot) => {
-  const data = snapshot.data();
+document
+  .getElementById("createRoomBtn")
+  .addEventListener("click", createRoom);
 
-  if (data?.joined) {
-    statusEl.textContent = "Phone Connected ✅";
-  }
-  document.getElementById("alpha").textContent = data.alpha ?? 0;
+async function createRoom() {
 
-  document.getElementById("beta").textContent = data.beta ?? 0;
+  const roomId = Math.random()
+    .toString(36)
+    .substring(2, 8)
+    .toUpperCase();
 
-  document.getElementById("gamma").textContent = data.gamma ?? 0;
-});
+  document.getElementById("roomId").textContent =
+    roomId;
+
+  await setDoc(
+    doc(db, "rooms", roomId),
+    {
+      joined: false,
+      createdAt: Date.now()
+    }
+  );
+
+  const joinUrl =
+    `${location.origin}${location.pathname.replace("index.html","")}controller.html?room=${roomId}`;
+
+  document.getElementById("qrcode").innerHTML = "";
+
+  new QRCode(
+    document.getElementById("qrcode"),
+    joinUrl
+  );
+
+  console.log(joinUrl);
+
+  onSnapshot(
+    doc(db, "rooms", roomId),
+    (snapshot) => {
+
+      const data = snapshot.data();
+
+      if (!data) return;
+
+      if (data.joined) {
+        statusEl.textContent =
+          "Phone Connected ✅";
+      }
+
+      document.getElementById("alpha").textContent =
+        data.alpha ?? 0;
+
+      document.getElementById("beta").textContent =
+        data.beta ?? 0;
+
+      document.getElementById("gamma").textContent =
+        data.gamma ?? 0;
+    }
+  );
+}
